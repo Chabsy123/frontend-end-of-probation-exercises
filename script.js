341f8de8969d788eab5126510205ccf8ca83b27b
Chukwu Sydney <chukwusydney061@gmail.com>
2025-05-23 17:32:09 +0100

    Search for Products and Product View

--- script.js
+++ script.js
@@ -55,3 +55,36 @@ document.querySelector('.search-form input').addEventListener('input',(e) => {
 
 });
 
+//Category filter
+document.querySelectorAll('categories input').forEach(box => {
+    box.addEventListener('change', filterProducts);
+});
+
+// Filter main 
+function filterProducts (){
+    const checked = [document.querySelectorAll('.categories input:checked')].map(box => box.nextElementSibling.textContent);
+//tenary operator filter
+    const filtered = checked.length == 0
+    ? products
+    : products.filter(p => checked.includes(p.category));
+
+    showProducts(filtered);
+
+}
+
+// Product view
+
+document.querySelector('sort-section select').addEventListener('change', (e) => {
+    const sorted = [...products];
+    const value = e.target.value;
+
+    if (value === 'price-asc') sorted.sort((a, b) => a.price - b.price);
+    if (value === 'price-desc') sorted.sort((a, b) => b.price - a.price);
+    if (value === 'name-asc') sorted.sort((a, b) =>a.title.localeCompare(b.title));
+    if (value === 'name-desc') sorted.sort((a, b) =>b.title.localeCompare(a.title));
+
+    showProducts(sorted);
+})
+
+//Initialise
+showProducts(products);
